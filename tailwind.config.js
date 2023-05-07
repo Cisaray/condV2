829e1b2ec4b302b2d2e965be89d76a802b1ec856
Kirill <lyadovkzst@gmail.com>
2023-05-07 17:27:45 +0500

    new confencionery

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        screens: {
+            lg: {max: '1199.99px'},
+            md: {max: '991.99px'},
+            sm: {max: '767.99px'},
+            xs: {max: '479.99px'},
+        },
+        extend: {
+            fontFamily: {
+                playfair: ['Playfair Display', 'serif'],
+            },
+            backgroundImage: {
+                mainBg: '#f0e5d1',
+                firstBgImg: 'url(../public/assets/bg1.JPG)'
+            },
+        },
+    },
+    plugins: [],
+}
+
